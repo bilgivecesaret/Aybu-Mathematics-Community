48a2be4e118c8d1d3f94853775f8456ea4adb638
Uğur ÖZKAN <bilgivecesaret@gmail.com>
2025-05-07 01:31:53 +0300

    Update weather.js

--- js/weather.js
+++ js/weather.js
@@ -1,18 +1,18 @@
 const apiKey = "4242403e2d70e46b2deeb16e18ab1a30";
 const city = "Ankara";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=tr&units=metric`;
+const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&lang=tr&units=metric`;
 
-  fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error("HTTP Hatası: " + response.status);
-      }
-      return response.json();
-    })
-    .then(data => {
-      const icon = data.weather[0].icon;
-      const description = data.weather[0].description;
-      const weather = `
+fetch(url)
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error("HTTP Error: " + response.status);
+    }
+    return response.json();
+  })
+  .then((data) => {
+    const icon = data.weather[0].icon;
+    const description = data.weather[0].description;
+    const weather = `
         <tr>
           <td>
             <p class="country">
@@ -27,9 +27,10 @@ const city = "Ankara";
           </td>
         </tr>
       `;
-      document.getElementById("weather").innerHTML = weather;
-    })
-    .catch(error => {
-      console.error("Hava durumu verisi alınamadı:", error);
-      document.getElementById("weather").innerHTML = "<tr><td colspan='2'>Hava durumu bilgisi alınamadı.</td></tr>";
-    });
+    document.getElementById("weather").innerHTML = weather;
+  })
+  .catch((error) => {
+    console.error("Weather data could not be obtained:", error);
+    document.getElementById("weather").innerHTML =
+      "<tr><td colspan='2'>Weather data could not be obtained.</td></tr>";
+  });
